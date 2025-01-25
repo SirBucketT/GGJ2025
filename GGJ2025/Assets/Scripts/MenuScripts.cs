21ae3f09180c1235ada378bd3509ff87ec9a7516
SirBucketT <tkpriva@icloud.com>
2025-01-25 11:02:12 +0100

    menu script update & credits button created

--- GGJ2025/Assets/Scripts/MenuScripts.cs
+++ GGJ2025/Assets/Scripts/MenuScripts.cs
@@ -6,6 +6,28 @@ using System.Collections.Generic;
 
 public class MenuScripts : MonoBehaviour
 {
+    
+    [SerializeField] GameObject MainMenu;
+    [SerializeField] GameObject Credits;
+
+    public void creditsButton()
+    {
+        if (MainMenu.activeInHierarchy)
+        {
+            MainMenu.SetActive(false);
+            Credits.SetActive(true);
+        }
+    }
+
+    public void MenuButton()
+    {
+        if (!MainMenu.activeInHierarchy)
+        {
+            MainMenu.SetActive(true);
+            Credits.SetActive(false);
+        }
+    }
+    
     public void StartGame()
     {
         SceneManager.LoadScene(1);

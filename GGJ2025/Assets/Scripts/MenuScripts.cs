2227c4dcc960cf5cceae42f397bea542200f7433
SirBucketT <tkpriva@icloud.com>
2025-01-25 10:46:53 +0100

    start game method in menu script implemented

--- GGJ2025/Assets/Scripts/MenuScripts.cs
+++ GGJ2025/Assets/Scripts/MenuScripts.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MenuScripts : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void StartGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(1);
     }
 }

fb3fe43ea336278cac07bf2296d726dce86b0f79
SirBucketT <tkpriva@icloud.com>
2025-01-25 14:03:26 +0100

    fixing sound settings

--- GGJ2025/Assets/Scripts/SoundSetter.cs
+++ GGJ2025/Assets/Scripts/SoundSetter.cs
@@ -1,14 +1,49 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class SoundSetter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    [Header("Audio Mixer")]
+    [SerializeField] AudioMixer audioMixer;
+    
+    [Header("UI Sliders")]
+    [SerializeField] Slider musicVolumeSlider;
+    [SerializeField] Slider effectsVolumeSlider;
+    
+    private const string MusicVolumeParam = "MusicVolume";
+    private const string EffectsVolumeParam = "EffectsVolumeParam";
+    
     void Start()
     {
+        float defaultVolume = 0f;  
+        float musicVol  = PlayerPrefs.GetFloat(MusicVolumeParam, defaultVolume);
+        float sfxVol    = PlayerPrefs.GetFloat(EffectsVolumeParam, defaultVolume);
         
+        musicVolumeSlider.value = musicVol;
+        effectsVolumeSlider.value   = sfxVol;
+        
+        audioMixer.SetFloat(MusicVolumeParam,  musicVol);
+        audioMixer.SetFloat(EffectsVolumeParam,    sfxVol);
+        
+        musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
+        effectsVolumeSlider.onValueChanged.AddListener(SetSfxVolume);
+        
+        void SetMusicVolume(float value)
+        {
+            audioMixer.SetFloat(MusicVolumeParam, value);
+            PlayerPrefs.SetFloat(MusicVolumeParam, value);
+        }
+        
+        void SetSfxVolume(float value)
+        {
+            audioMixer.SetFloat(EffectsVolumeParam, value);
+            PlayerPrefs.SetFloat(EffectsVolumeParam, value);
+        }
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
